f203c16a964f098651f4a8011d3ef6abe1d652d7
venus-snuev <mylastlxl@outlook.com>
2021-09-09 01:02:59 +0800

    第二次提交

--- my-git-test/src/main/java/com/xl/HelloGit.java
+++ my-git-test/src/main/java/com/xl/HelloGit.java
@@ -3,5 +3,9 @@ package com.xl;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello-Git!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello");
+        }
     }
+
 }
